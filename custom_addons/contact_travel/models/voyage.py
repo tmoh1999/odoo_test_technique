a98471cb2e3814a34515981d26508444d0ec68dc
touabti mohamed <mohamedtouabti1999@gmail.com>
2023-12-23 23:29:07 +0100

    added fields name,datedepart,destination to voyage model.

--- custom_addons/contact_travel/models/voyage.py
+++ custom_addons/contact_travel/models/voyage.py
@@ -2,7 +2,12 @@ from odoo import api, fields, models, _, tools
 from odoo.osv import expression
 from odoo.exceptions import UserError, ValidationError
 
-
+#modèle voyage class
 class Voyage(models.Model):
     _name = "voyage"
     _description = "Voyage"
+    
+    #les attributs de la class Voyage 
+    name= fields.Char(string="Nom du Voyage")
+    dateDepart=fields.Date(string="Date de départ")
+    destination=fields.Char(string="Destination")
