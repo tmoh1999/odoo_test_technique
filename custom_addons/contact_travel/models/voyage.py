0172372f909c2aaf3e0a75efb5ee4fbaf933ac08
touabti mohamed <mohamedtouabti1999@gmail.com>
2023-12-24 18:15:48 +0100

    added many2one link in voyage model to res.partner model.

--- custom_addons/contact_travel/models/voyage.py
+++ custom_addons/contact_travel/models/voyage.py
@@ -2,12 +2,16 @@ from odoo import api, fields, models, _, tools
 from odoo.osv import expression
 from odoo.exceptions import UserError, ValidationError
 
-#modèle voyage class
+#Voyage Model class 
 class Voyage(models.Model):
     _name = "voyage"
     _description = "Voyage"
     
-    #les attributs de la class Voyage 
+    #Voyage Model Class attributes 
     name= fields.Char(string="Nom du Voyage")
     dateDepart=fields.Datetime(string="Date de départ", default=fields.Datetime.now)
     destination=fields.Char(string="Destination")
+    
+    #Many2one attribute : a link between res.partner Model and Voyage Model 
+    #res.partner Contact can have a list of voyages .     
+    voyageur_id=fields.Many2one('res.partner', string='Contact')
