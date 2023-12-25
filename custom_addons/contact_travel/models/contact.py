0a88ac1904e8dc9fce8694ff5fc8c225bae66a2f
touabti mohamed <mohamedtouabti1999@gmail.com>
2023-12-25 05:44:41 +0100

    added a new function <liste_voyage> in contact.py (same name as button in related view) to liste voyages of contact.

--- custom_addons/contact_travel/models/contact.py
+++ custom_addons/contact_travel/models/contact.py
@@ -5,4 +5,21 @@ from odoo.exceptions import UserError, ValidationError
 #Contact Model class inherit from res.partner Model 
 class Contact(models.Model):
     _inherit = "res.partner"
-	
+
+    def liste_voyages(self):
+        field_ids = self.env['voyage'].search([('voyageur_id','=',self.id)]).ids
+        
+        domain = [('id','in',field_ids)]
+
+        view_id_tree = self.env['ir.ui.view'].search([('name','=',"voyage.tree")])
+        return {
+            'name':"Liste des Voyages",
+            'type': 'ir.actions.act_window',
+            'res_model': 'voyage',
+            'view_type': 'form',
+            'view_mode': 'tree,form',
+            'views': [(view_id_tree[0].id, 'tree'),(False,'form')],
+            'view_id ref="contact_travel.tree_view"': '',
+            'target': 'current',
+            'domain': domain,
+        }
